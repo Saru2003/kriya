e2298013a088b2a1c50cab5a60fbd9377196ce16
Sarveshwaar SS <94345639+Saru2003@users.noreply.github.com>
2023-04-11 12:02:11 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("kriya.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open("kriya").sheet1 
+sheet = client.open("psg_bank").sheet1 
 # data = sheet.get_all_records()
 st.set_page_config(page_title="Login page",layout="wide")
 st.write(
